28d4e6d6c42fc4d4705f3d95037e2a5161c456cc
Karen Jully <karenjully.kr15@gmail.com>
2022-06-10 23:33:10 -0300

    splashActivity

---
+++ app/src/main/java/br/com/authentication/SplashActivity.kt
@@ -0,0 +1,26 @@
+package br.com.authentication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import br.com.authentication.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
