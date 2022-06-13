4aad3bcf69238ee5764335a2d70b2bf9e3da5da1
Karen Jully <karenjully.kr15@gmail.com>
2022-06-12 23:16:12 -0300

    alteração da splash

--- app/src/main/java/br/com/authentication/activity/SplashActivity.kt
+++ app/src/main/java/br/com/authentication/activity/SplashActivity.kt
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.widget.ImageView
+import br.com.authentication.R
 import br.com.authentication.databinding.ActivitySplashBinding
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
@@ -33,6 +37,13 @@ class SplashActivity : AppCompatActivity() {
                 finish()
             }
         }, 3000)
+
+        showGif()
+    }
+
+    private fun showGif() {
+        val imageView: ImageView = binding.imgSplash
+        Glide.with(this).load(R.drawable.ic_firebase).into(imageView)
     }
 
 }
